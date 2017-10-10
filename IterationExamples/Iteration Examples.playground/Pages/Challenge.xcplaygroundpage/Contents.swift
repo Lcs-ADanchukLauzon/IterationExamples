170abbc88bc2638ad293fc7b6f18a500176ace02
Danchuk-Lauzon, Alexy <adanchuklauzon@lcs-mba3080.lcs.on.ca>
2017-10-10 09:42:15 -0400

    loops challenge one

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -13,18 +13,16 @@ import PlaygroundSupport
 
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
-canvas.defaultLineWidth = 5
+canvas.defaultLineWidth = 3
+canvas.translate(byX: 150, byY: 150)
 
-for i in stride(from: 0, to: 300, by: 20) {
+for i in stride(from: 0, to: 360, by: 9) {
     
     canvas.lineColor = Color.init(hue: i, saturation: 100, brightness: 100, alpha: 100)
 
-    
-
-    canvas.drawLine(fromX: i, fromY: 0, toX: 300, toY: i)
-     canvas.drawLine(fromX: 300, fromY: i, toX: 300-i, toY: 300)
-      canvas.drawLine(fromX: 300-i, fromY: 300, toX: 0, toY: 300-i)
-     canvas.drawLine(fromX: 0, fromY: 300-i, toX: i, toY: 0)
+    canvas.rotate(by: Degrees(i))
+    canvas.drawLine(fromX: 0, fromY: 0, toX: 100, toY: 0)
+    canvas.rotate(by: Degrees(-i))
 }
 
 
