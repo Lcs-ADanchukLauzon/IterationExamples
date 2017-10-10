840f35e51316afee451572e29d8fc0ad731fbc44
Danchuk-Lauzon, Alexy <adanchuklauzon@lcs-mba3080.lcs.on.ca>
2017-10-10 09:56:20 -0400

    octagon

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -16,17 +16,16 @@ let canvas = Canvas(width: 300, height: 300)
 canvas.defaultLineWidth = 3
 canvas.translate(byX: 150, byY: 150)
 
-for i in stride(from: 0, to: 360, by: 9) {
+for i in stride(from: 0, to: 360, by: 45) {
     
     canvas.lineColor = Color.init(hue: i, saturation: 100, brightness: 100, alpha: 100)
-
-    canvas.rotate(by: Degrees(i))
-    canvas.drawLine(fromX: 0, fromY: 0, toX: 100, toY: 0)
-    canvas.rotate(by: Degrees(-i))
+canvas.rotate(by: Degrees(i))
+    canvas.drawLine(fromX: -50, fromY: -122, toX: 50, toY: -122)
+  canvas.rotate(by: Degrees(-i))
 }
 
 
-
+// 135
 
 /*:
  ## Template code
