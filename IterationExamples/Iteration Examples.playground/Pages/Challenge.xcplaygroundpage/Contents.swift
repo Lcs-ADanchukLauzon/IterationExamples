a1517d21dcb6cf951200525c68019d5d0b0abab8
Danchuk-Lauzon, Alexy <adanchuklauzon@lcs-mba3080.lcs.on.ca>
2017-10-12 13:25:47 -0400

    color dots (loops)

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -13,16 +13,18 @@ import PlaygroundSupport
 
 // Create canvas
 let canvas = Canvas(width: 300, height: 300)
-canvas.defaultLineWidth = 3
-canvas.translate(byX: 150, byY: 150)
 
-for i in stride(from: 0, to: 360, by: 8) {
-    
-    canvas.lineColor = Color.init(hue: i, saturation: 100, brightness: 100, alpha: 100)
-canvas.rotate(by: Degrees(i))
-    canvas.drawLine(fromX: -50, fromY: -100, toX: 50, toY: -100)
-  canvas.rotate(by: Degrees(-i))
-}
+
+for x in stride(from: 25, through: 275, by: 50){
+    for y in stride(from: 275, through: 25, by: -50){
+       canvas.fillColor = Color.init(hue: x, saturation: 100, brightness: 100, alpha: 100)
+        
+        canvas.drawEllipse(centreX: x, centreY: y, width: 5, height: 5)
+        
+        }
+    }
+
+
 
 
 // 135
